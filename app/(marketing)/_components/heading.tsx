949cb1f8fc8b198b46b04f584f08d50539139a65
krutulkamil <krutulkamil@onet.pl>
2024-03-03 10:07:06 +0100

    feat: marketing page init /w metadata

---
+++ app/(marketing)/_components/heading.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import React from 'react';
+
+export function Heading() {
+  return (
+    <div className="max-w-3xl space-y-4">
+      <h1>
+        Your Ideas, Documents & Plans. Unified. Welcome to{' '}
+        <span className="underline">Mesh</span>
+      </h1>
+    </div>
+  );
+}
