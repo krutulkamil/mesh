9af8bb747d5fb12ee6ca6e6bbe8a7464f1f037e3
krutulkamil <krutulkamil@onet.pl>
2024-03-18 19:53:56 +0100

    fix: sidebar collapse reset width on click

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -126,7 +126,11 @@ export function Navigation() {
       >
         <nav className="bg-transparent px-3 py-2 w-full">
           {isCollapsed && (
-            <MenuIcon role="button" className="h-6 w-6 text-muted-foreground" />
+            <MenuIcon
+              onClick={resetWidth}
+              role="button"
+              className="h-6 w-6 text-muted-foreground"
+            />
           )}
         </nav>
       </div>
