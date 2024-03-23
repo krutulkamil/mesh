19a8a8216e5ac279043cedb43e082797cd48be2e
krutulkamil <krutulkamil@onet.pl>
2024-03-23 19:53:03 +0100

    feat: getSidebar server action

--- convex/documents.ts
+++ convex/documents.ts
@@ -1,6 +1,30 @@
 import { v } from 'convex/values';
 
-import { mutation } from './_generated/server';
+import { mutation, query } from './_generated/server';
+
+export const getSidebar = query({
+  args: {
+    parentDocument: v.optional(v.id('documents')),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error('Not Authenticated');
+    }
+
+    const userId = identity.subject;
+
+    return await ctx.db
+      .query('documents')
+      .withIndex('by_user_parent', (query) =>
+        query.eq('userId', userId).eq('parentDocument', args.parentDocument)
+      )
+      .filter((query) => query.eq(query.field('isArchived'), false))
+      .order('desc')
+      .collect();
+  },
+});
 
 export const create = mutation({
   args: {
