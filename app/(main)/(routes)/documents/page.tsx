3139bb8cdb29a065ed752bddba270f1547faa2bb
krutulkamil <krutulkamil@onet.pl>
2024-03-23 17:44:22 +0100

    feat: create new document action

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -3,12 +3,26 @@
 import React from 'react';
 import Image from 'next/image';
 import { useUser } from '@clerk/clerk-react';
+import { useMutation } from 'convex/react';
+import { toast } from 'sonner';
 import { PlusCircle } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
+import { api } from '@/convex/_generated/api';
 
 export default function DocumentsPage() {
   const { user } = useUser();
+  const create = useMutation(api.documents.create);
+
+  function onCreate() {
+    const promise = create({ title: 'Untitled' });
+
+    toast.promise(promise, {
+      loading: 'Creating a new note',
+      success: 'New note created!',
+      error: 'Failed to create a new note.',
+    });
+  }
 
   return (
     <div className="h-full flex flex-col items-center justify-center space-y-4">
@@ -29,7 +43,7 @@ export default function DocumentsPage() {
       <h2 className="text-lg font-medium">
         Welcome to {user?.firstName ?? user?.username}&apos;s Mesh
       </h2>
-      <Button onClick={() => {}}>
+      <Button onClick={onCreate}>
         <PlusCircle className="h-4 w-4 mr-2" />
         Create a note
       </Button>
