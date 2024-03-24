0f21a6608caf6570cbf806e8e4bd5a5946645a9e
krutulkamil <krutulkamil@onet.pl>
2024-03-24 17:45:10 +0100

    feat: recursive archive children documents

--- convex/documents.ts
+++ convex/documents.ts
@@ -1,6 +1,7 @@
 import { v } from 'convex/values';
 
 import { mutation, query } from './_generated/server';
+import type { Id } from './_generated/dataModel';
 
 export const archive = mutation({
   args: { id: v.id('documents') },
@@ -23,9 +24,34 @@ export const archive = mutation({
       throw new Error('Unauthorized');
     }
 
-    return await ctx.db.patch(args.id, {
+    const recursiveArchive = async (documentId: Id<'documents'>) => {
+      const children = await ctx.db
+        .query('documents')
+        .withIndex('by_user_parent', (query) =>
+          query.eq('userId', userId).eq('parentDocument', documentId)
+        )
+        .collect();
+
+      await ctx.db.patch(documentId, {
+        isArchived: true,
+      });
+
+      for (const child of children) {
+        await ctx.db.patch(child._id, {
+          isArchived: true,
+        });
+
+        await recursiveArchive(child._id);
+      }
+    };
+
+    const document = await ctx.db.patch(args.id, {
       isArchived: true,
     });
+
+    await recursiveArchive(args.id);
+
+    return document;
   },
 });
 
