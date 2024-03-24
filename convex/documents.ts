43e212022a74b8fd5f91efa739c64cef05bbaa29
krutulkamil <krutulkamil@onet.pl>
2024-03-24 18:51:21 +0100

    feat: restore documents server mutation

--- convex/documents.ts
+++ convex/documents.ts
@@ -1,7 +1,7 @@
 import { v } from 'convex/values';
 
 import { mutation, query } from './_generated/server';
-import type { Id } from './_generated/dataModel';
+import type { Doc, Id } from './_generated/dataModel';
 
 export const archive = mutation({
   args: { id: v.id('documents') },
@@ -119,5 +119,71 @@ export const getTrash = query({
       .filter((query) => query.eq(query.field('isArchived'), true))
       .order('desc')
       .collect();
+  },
+});
+
+export const restore = mutation({
+  args: { id: v.id('documents') },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error('Not Authenticated');
+    }
+
+    const userId = identity.subject;
+
+    const existingDocument = await ctx.db.get(args.id);
+
+    if (!existingDocument) {
+      throw new Error('Not Found');
+    }
+
+    if (existingDocument.userId !== userId) {
+      throw new Error('Unauthorized');
+    }
+
+    const recursiveRestore = async (documentId: Id<'documents'>) => {
+      const children = await ctx.db
+        .query('documents')
+        .withIndex('by_user_parent', (query) =>
+          query.eq('userId', userId).eq('parentDocument', documentId)
+        )
+        .collect();
+
+      for (const child of children) {
+        await ctx.db.patch(child._id, {
+          isArchived: false,
+        });
+
+        await recursiveRestore(child._id);
+      }
+    };
+
+    const options: Partial<Doc<'documents'>> = {
+      isArchived: false,
+    };
+
+    if (existingDocument.parentDocument) {
+      const parent = await ctx.db.get(existingDocument.parentDocument);
+
+      if (!parent) {
+        throw new Error('Not Found');
+      }
+
+      if (parent.userId !== userId) {
+        throw new Error('Unauthorized');
+      }
+
+      if (parent.isArchived) {
+        options.parentDocument = undefined;
+      }
+    }
+
+    await ctx.db.patch(args.id, options);
+
+    await recursiveRestore(args.id);
+
+    return existingDocument;
   },
 });
