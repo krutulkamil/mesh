5f9afc1a0f29af8c7d1c67b3c0ef1b9293e5cdbe
krutulkamil <krutulkamil@onet.pl>
2024-04-07 19:14:57 +0200

    feat: navbar banner component

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -5,6 +5,7 @@ import { MenuIcon } from 'lucide-react';
 
 import { api } from '@/convex/_generated/api';
 import { Title } from '@/app/(main)/_components/title';
+import { Banner } from '@/app/(main)/_components/banner';
 import type { Id } from '@/convex/_generated/dataModel';
 
 interface NavbarProps {
@@ -19,23 +20,30 @@ export function Navbar({ isCollapsed, onResetWidth }: Readonly<NavbarProps>) {
   });
 
   if (document === undefined) {
-    return <p>Loading...</p>;
+    return (
+      <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center justify-between">
+        <Title.Skeleton />
+      </nav>
+    );
   }
 
   if (document === null) return null;
 
   return (
-    <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center gap-x-4">
-      {isCollapsed && (
-        <MenuIcon
-          role="button"
-          onClick={onResetWidth}
-          className="h-6 w-6 text-muted-foreground"
-        />
-      )}
-      <div className="flex items-center justify-between w-full">
-        <Title initialData={document} />
-      </div>
-    </nav>
+    <>
+      <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center gap-x-4">
+        {isCollapsed && (
+          <MenuIcon
+            role="button"
+            onClick={onResetWidth}
+            className="h-6 w-6 text-muted-foreground"
+          />
+        )}
+        <div className="flex items-center justify-between w-full">
+          <Title initialData={document} />
+        </div>
+      </nav>
+      {document.isArchived && <Banner documentId={document._id} />}
+    </>
   );
 }
